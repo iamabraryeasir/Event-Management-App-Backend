0ad4ee7bf1c8f556e4a48f16e1c622179252719d
Abrar Yeasir <iamabraryeasir@gmail.com>
2025-04-23 02:02:13 +0600

    feat: implement getEventService to retrieve event details with user information

--- src/services/event/getEvent.js
+++ src/services/event/getEvent.js
@@ -1,11 +1,45 @@
 import createHttpError from "http-errors";
 import { ApiResponse } from "../../helpers/ApiResponse.js";
 import { Event } from "../../models/event.model.js";
-import { User } from "../../models/user.model.js";
+import mongoose from "mongoose";
 
 export const getEventService = async (req, res, next) => {
   try {
-    res.send("Get Event");
+    const eventId = req.params.id;
+
+    const [event] = await Event.aggregate([
+      { $match: { _id: new mongoose.Types.ObjectId(eventId) } },
+      { $project: { updatedAt: 0 } },
+      {
+        $lookup: {
+          from: "users",
+          localField: "user",
+          foreignField: "_id",
+          as: "createdBy",
+        },
+      },
+      { $unwind: "$createdBy" },
+      {
+        $project: {
+          "createdBy._id": 1,
+          "createdBy.name": 1,
+          name: 1,
+          description: 1,
+          date: 1,
+          location: 1,
+          banner: 1,
+          createdAt: 1,
+        },
+      },
+    ]);
+    
+    if (!event) {
+      return next(createHttpError(404, "No event found"));
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "Event retrieved successfully", event));
   } catch (error) {
     return next(createHttpError(500, "Internal server error getting event"));
   }
