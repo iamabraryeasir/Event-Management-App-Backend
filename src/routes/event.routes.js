80b87470936582035da51adca3fdd82e7f2cc688
Abrar Yeasir <iamabraryeasir@gmail.com>
2025-04-23 01:56:18 +0600

    feat: implement event creation with file upload and validation

--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -8,13 +8,14 @@ import {
   deleteEvent,
 } from "../controllers/event.controller.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 router.get("/", getEvents);
 router.get("/:id", getEvent);
 
-router.post("/", protectRoute, createEvent);
+router.post("/", protectRoute, upload.single("banner"), createEvent);
 router.put("/:id", protectRoute, updateEvent);
 router.delete("/:id", protectRoute, deleteEvent);
 
--- src/services/event/createEvent.js
+++ src/services/event/createEvent.js
@@ -2,10 +2,39 @@ import createHttpError from "http-errors";
 import { ApiResponse } from "../../helpers/ApiResponse.js";
 import { Event } from "../../models/event.model.js";
 import { User } from "../../models/user.model.js";
+import { uploadToCloudinary } from "../../helpers/cloudinary.js";
 
 export const createEventService = async (req, res, next) => {
   try {
-    res.send("Create Event");
+    const { name, description, date, location } = req.body;
+    if (!name?.trim() || !description?.trim() || !date || !location?.trim()) {
+      return next(createHttpError(400, "All fields are required"));
+    }
+
+    const user = req.user;
+    if (!user) {
+      return next(createHttpError(401, "Unauthorized - Login required"));
+    }
+
+    const userExists = await User.findById(user.id);
+    if (!userExists) {
+      return next(createHttpError(401, "Unauthorized - Login required"));
+    }
+
+    const banner = await uploadToCloudinary(req.file.path, "event-banners");
+
+    const event = await Event.create({
+      name,
+      description,
+      date,
+      location,
+      banner: banner?.secure_url,
+      user: user.id,
+    });
+
+    return res
+      .status(201)
+      .json(new ApiResponse(201, "Event created successfully", event));
   } catch (error) {
     return next(createHttpError(500, "Internal server error creating event"));
   }
