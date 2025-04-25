4194fadab75edb7bd18aaa028fdd9b407c788b50
Abrar Yeasir <iamabraryeasir@gmail.com>
2025-04-25 15:43:22 +0600

    redy for deploy on vercel

--- src/app.js
+++ src/app.js
@@ -39,17 +39,17 @@ app.use(
 // middlewares
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
-app.use(cookieParser()); 
+app.use(cookieParser());
 app.use(expressMongoSanitize());
-app.use(helmet()); 
-app.use(hpp()); 
-app.use(xss()); 
+app.use(helmet());
+app.use(hpp());
+app.use(xss());
 
 // serving static files
 app.use(express.static("public"));
 
 // setting base route
-app.use("/api/v1", api);
+app.use("/api", api);
 
 // global error handler
 app.use(globalErrorHandler);
